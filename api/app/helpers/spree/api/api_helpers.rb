18b83996da122fbdfc0334f6b7bfb022580b391a
bouafia19 <khaledsetif1900@gmail.com>
2021-08-07 10:30:19 +0100

    add store manager role to users

--- api/app/helpers/spree/api/api_helpers.rb
+++ api/app/helpers/spree/api/api_helpers.rb
@@ -138,7 +138,7 @@ module Spree
         :id, :month, :year, :cc_type, :last_digits, :name
       ]
 
-      @@user_attributes = [:id, :email, :created_at, :updated_at, :spree_api_key, :spree_role_ids]
+      @@user_attributes = [:id, :email, :created_at, :updated_at, :spree_api_key, :spree_role_ids, :store_manager_id]
 
       @@property_attributes = [:id, :name, :presentation]
 
