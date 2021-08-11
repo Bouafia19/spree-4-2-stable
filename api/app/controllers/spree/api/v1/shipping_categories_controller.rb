273439d889817fcfc04840e2d27f7de4aace5af9
bouafia19 <khaledsetif1900@gmail.com>
2021-08-11 07:01:47 +0100

    add index method to Shipping_categories_controller

--- api/app/controllers/spree/api/v1/shipping_categories_controller.rb
+++ api/app/controllers/spree/api/v1/shipping_categories_controller.rb
@@ -2,6 +2,10 @@ module Spree
     module Api
         module V1
             class ShippingCategoriesController < Spree::Api::BaseController
+                def index
+                    @ShippingCategories = ShippingCategory.all
+                    render :json => @ShippingCategories
+                end
             end
         end
     end
