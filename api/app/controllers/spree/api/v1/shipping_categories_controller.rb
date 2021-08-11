f7fcf3304628f9d09ca92dedcb6d19fcc7996f61
bouafia19 <khaledsetif1900@gmail.com>
2021-08-11 11:14:33 +0100

    add CRUD to shipping categories controller

--- api/app/controllers/spree/api/v1/shipping_categories_controller.rb
+++ api/app/controllers/spree/api/v1/shipping_categories_controller.rb
@@ -3,8 +3,49 @@ module Spree
         module V1
             class ShippingCategoriesController < Spree::Api::BaseController
                 def index
-                    @ShippingCategories = ShippingCategory.all
-                    render :json => @ShippingCategories
+                    authorize! :index, ShippingCategory
+                    @shipping_category = ShippingCategory.accessible_by(current_ability).order('name ASC').
+                             ransack(params[:q])
+                    respond_with(@shipping_category)
+                end
+
+                def destroy
+                    authorize! :destroy, shipping_category
+                    shipping_category.destroy
+                    respond_with(shipping_category, status: 204)
+                end
+
+                def show
+                    respond_with(shipping_category)
+                end
+
+                def create
+                    authorize! :create, ShippingCategory
+                    @shipping_category = ShippingCategory.new(shipping_category_params)
+                    if @shipping_category.save
+                        respond_with(@shipping_category, status: 201, default_template: :show)
+                    else
+                        invalid_resource!(@shipping_category)
+                    end
+                end
+          
+                def update
+                    authorize! :update, shipping_category
+                    if shipping_category.update(shipping_category_params)
+                        respond_with(shipping_category, status: 200, default_template: :show)
+                    else
+                        invalid_resource!(shipping_category)
+                    end
+                end
+
+                private
+
+                def shipping_category
+                    @shipping_category ||= ShippingCategory.accessible_by(current_ability, :show).find(params[:id])
+                end
+
+                def shipping_category_params
+                    params.require(:shipping_category).permit(permitted_shipping_category_attributes)
                 end
             end
         end
